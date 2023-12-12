4d065e5f3402ac882aab55851f9ceba887e75593
zqtang-chn <zqtang.chn@gmail.com>
2023-12-12 12:19:55 +0800

    electron context and node func

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 - `fuzz-helper` afl-fuzz和libfuzzer的助手。涉及到C中的内存释放、cvector和位读工具。
 - `transformer` 熟悉transformer的代码论文和代码细节，训练一个中英翻译器。
 - `ffmpeg-fuzz` fuzz ffmpeg avdec的harness。 
-- ``
+- `electron-demo` 验证electron中上下文隔离功能和NodeJS通过IPC代理
 
 
 
---
+++ electron-demo/context-isolation/main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadFile('index.html');
+}
+
+ipcMain.handle('read-file', async (event, filePath) => {
+    try {
+      const data = fs.readFileSync(filePath, 'utf-8');
+      return data;
+    } catch (error) {
+      console.error('Failed to read file', error);
+      throw error;
+    }
+});
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
